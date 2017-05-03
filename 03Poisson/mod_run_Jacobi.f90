d42fae656d2d22548e85180af7cc6fbea83841b5
Enyaar <andreas_beckert@ymail.com>
2017-05-03 20:41:41 +0200

    Update mod_run_Jacobi.f90

--- 03Poisson/mod_run_Jacobi.f90
+++ 03Poisson/mod_run_Jacobi.f90
@@ -6,35 +6,29 @@ MODULE run
 		double precision, allocatable, dimension(:,:), intent(inout) :: matrix !matrix
 		double precision, allocatable, dimension(:,:) :: temp !temporäre Kopie
 		double precision :: star, corr, h !Hilfsvariable
-		integer :: diff = 0, counter = 0 !Zähler in Schleifen
-		logical, intent(out) :: checkExit
+		logical, intent(out) :: checkExit !Beendet Iteration in poisson.f90, falls = .true. am Ende der Subroutine
 		integer :: i, j !Schleifenvariablen
 
 		allocate(temp(ubound(matrix, 1), ubound(matrix, 2)))
+
+		checkExit = .true.
 		temp = matrix ! so müssen die Ränder nicht erneut belegt werden
 		h = 1.0 / (ubound(matrix, 1) - lbound(matrix, 1))
-		diff = 0
-		counter = 0
+
 		! Rechnet nicht an den Rändern
 		do j = lbound(matrix, 2)+1, ubound(matrix, 2)-1
 			do i = lbound(matrix, 1)+1, ubound(matrix, 1)-1
 				! Stern
 				star = -matrix(i,j+1)-matrix(i-1,j)+4*matrix(i,j)-matrix(i+1,j)-matrix(i,j-1)
 				! Korrektur
-				corr = matrix(i,j) * h * h - star/4
-				if(corr <= 10e-6) then
-					diff = diff +1
+				corr = -star/4
+				if(corr >= 10e-6) then
+					checkExit = .false.
 				endif
-				counter = counter +1
 				!Übertargen auf temporaere Matrix
 				temp(i,j) = matrix(i,j) + corr 
 			end do
 		end do
-
-		!Wenn für jeden Wert in der Matrix die Abbruchbedingung erfüllt ist:
-		if (diff == counter) then
-			checkExit = .True.  !beendet die Schleife in poission.f90
-		endif
 
 		! Übertrag auf aktuelle Matrix auch wenn die Abbruchbedingung erfüllt ist,
 		! da die "zu genaue" Matrix sowieso berechnet wurde 
