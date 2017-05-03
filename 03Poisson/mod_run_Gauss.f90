55ef295ba46670b9fd21182a00196306ad006347
Enyaar <andreas_beckert@ymail.com>
2017-05-03 20:41:12 +0200

    Update mod_run_Gauss.f90

--- 03Poisson/mod_run_Gauss.f90
+++ 03Poisson/mod_run_Gauss.f90
@@ -4,42 +4,27 @@ MODULE run
 	
 	subroutine calculate(matrix,checkExit) !more parameters as needed
 		double precision, dimension(:,:), intent(inout) :: matrix
-
 		double precision :: star, corr, h
-		integer :: diff = 0, counter = 0
-		logical, intent(out) :: checkExit
+		logical, intent(out) :: checkExit !Beendet Iteration in poisson.f90, falls = .true. am Ende der Subroutine
 		integer :: i,j !Schleifenvariablen
 
-
-
+		checkExit = .true.
 		h = 1.0 / (ubound(matrix, 1) - lbound(matrix, 1))
-		diff = 0
-		counter = 0
+
 		! Rechnet nicht an den Rändern
 		do j = lbound(matrix, 2)+1, ubound(matrix, 2)-1
 			do i = lbound(matrix, 1)+1, ubound(matrix, 1)-1
 				! Stern
-				star = -matrix(i,j+1)-matrix(i-1,j)+4*matrix(i,j)-matrix(i+1,j)-matrix(i,j-1)
+				star = (-matrix(i,j+1))-matrix(i-1,j)+4*matrix(i,j)-matrix(i+1,j)-matrix(i,j-1)
 				! Korrektur
-				corr = matrix(i,j) * h * h - star/4
-				if(corr <= 10e-6) then
-					diff = diff +1
-				endif
-				counter = counter +1
+				corr = -star/4
+				if (corr >= 10e-6) then
+					checkExit = .false.
+				end if
 				! Übertrag auf aktuelle Matrix
 				matrix(i,j) = matrix(i,j) + corr
 			end do
 		end do
-
-		!Wenn für jeden Wert in der Matrix die Abbruchbedingung erfüllt ist:
-		if (diff == counter) then
-			checkExit = .True. !beendet die Schleife in poission.f90
-		endif
-
-
-
-
-
 
 	end subroutine calculate
 
