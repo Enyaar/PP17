1a2cdc4505e4970bac39d0f7000619e598222bdf
Enyaar <andreas_beckert@ymail.com>
2017-06-25 14:34:01 +0200

    Update mod_finalize.f90

--- aufgabe08/08a/mod_finalize.f90
+++ aufgabe08/08a/mod_finalize.f90
@@ -1,73 +1,66 @@
-MODULE finalize
+program poisson
 	use mpi
-	IMPLICIT NONE
-	CONTAINS
-	
-	! subroutine printNumber	Ausgabe einzelnes Double auf Konsole
-	! subroutine printRow		Ausgabe einer Zeile auf Konsole
-	
-	
-	! gibt einzelnen Wert auf Konsole aus
-	subroutine printNumber(x)
-		double precision, intent(in) :: x
-		
-		! Typumwandlung zu Real um Nachkommastellen zu schneiden
-		write (*,1, advance='no') real(x)
-		! f10.8 heisst: 10 Stellen für die ganze Zahl, 8 für Nachkommastellen
-		1	format(f5.3)
-		! Abstand
-		write (*, '(A)', advance='no') "  "
-	end subroutine printNumber
-	
-	
-	! Prints one Row, only one Number every <interlines> rows
-	subroutine printRow(matrix, row, interlines)
-		double precision, dimension(:,:), allocatable, intent(in) :: matrix
-		integer, intent(in) :: row ! Row to be printed
-		integer, intent(in) :: interlines ! Size of gap between printed numbers
-		integer :: i ! Schleifenvariable
+	use initialize
+	use run
+	use finalize
+	!
+	implicit none
+	!
+	! parameter:
+	integer, parameter :: edgeLength = 17	! Kantenlänge der gesamten Matrix
+	!Erlaubte outputSize-möglichkeiten (bei edgelength = 97): 3, 5, 9, 24, 47, 93, 185
+	integer, parameter :: outputSize = 17	! Kantenlänge der ausgegebenen Matrix
+	integer, parameter :: loopSize = 100000	! Anzahl der Iterationen
+	! mpi:
+	integer :: mpi_rank, mpi_size, tag, status(MPI_STATUS_SIZE), mpi_ierr !MPI
+	integer :: mpi_master, mpi_last ! Besondere Prozess IDs
+	! data:
+	double precision, dimension(:,:), allocatable :: matrix ! or whatever fits
+	!double precision, dimension(:,:), allocatable :: temp
+	! auxiliary
+	integer :: lineCount ! die Anzahl der Zeilen in der einzelnen Matrix
+	integer :: offset ! = Index der ersten eigenen Zeile in Gesamtmatrix
+	logical :: checkExit =.False. ! Abbruchbedingung
+	integer :: i, j !Schleifenvariablen
+	! für Ausgabe	
+	integer :: interlines ! Abstand zwischen Zeilen, die ausgegeben werden sollen
+	integer :: currentRow = 1 ! Laufender Zähler für Ausgabe über alle Prozesse
+	interlines = (edgeLength-1) / (outputSize - 1)
 
-		
-		call printNumber(matrix(1,row))
-		do i = 2, ubound(matrix, 1)
-			if (mod(i-1, interlines) == 0) then
-				call printNumber(matrix(i,row))
-			endif
-		end do
-		print *, ""
-	end subroutine printRow
+	! INIT
+	call MPI_INIT(mpi_ierr)
+	call MPI_COMM_SIZE(MPI_COMM_WORLD,mpi_size,mpi_ierr)
+	call MPI_COMM_RANK(MPI_COMM_WORLD,mpi_rank,mpi_ierr)
 
-	subroutine printMatrix(matrix, interlines, offset, lineCount, mpi_rank, mpi_master, mpi_last, mpi_ierr)
-		integer, intent(in) :: mpi_rank, mpi_master, mpi_last, mpi_ierr ! mpi integer
-		integer :: status(MPI_STATUS_SIZE)
-		double precision, dimension(:,:), allocatable, intent(in) :: matrix
-		integer, intent(in) :: interlines, offset, lineCount ! Abstand zwischen Zeilen, die ausgegeben werden sollen und offset
-		integer :: currentRow = 1 ! Laufender Zähler für Ausgabe über alle Prozesse
-
-
-		if (mpi_rank == mpi_master) then
-			! Prolog:
-			print *, ""
-			print *, "Fertige Matrix"
-			! Dann alles andere (eventuell)
-			do while (currentRow < (offset + lineCount))
-				call printRow(matrix, currentRow, interlines)
-				currentRow = currentRow + interlines
-			end do
-			!print*, matrix
-			call mpi_send(currentRow, 1, mpi_integer, 1, 2017, mpi_comm_world, mpi_ierr)
-		else 
-			call mpi_recv(currentRow, 1, mpi_integer, (mpi_rank - 1), 2017, mpi_comm_world, status, mpi_ierr)
-		
-			do while (currentRow <= (offset + lineCount))
-				call printRow(matrix, (currentRow - offset + 1), interlines)
-				currentRow = currentRow + interlines
-			end do
-			!print*, matrix
-			if (mpi_rank .ne. mpi_last) then
-				call mpi_send(currentRow, 1, mpi_integer, (mpi_rank + 1), 2017, mpi_comm_world, mpi_ierr)
-			endif
+	! Fehlerabfrage
+	mpi_master = 0
+	mpi_last = mpi_size - 1
+	if (mpi_rank == mpi_master) then
+		if (mod((edgeLength-1),(outputSize-1)) /= 0) then
+			write(*,*) "Die Variable outputSize muss so gewählt werden, dass bei der ", & 
+			&		  "Rechnung [edgeLength-1 / ( outputSize - 1 )] kein Rest entsteht"
+			stop
 		endif
-	end subroutine printMatrix
+	endif
 
-END MODULE finalize
+	! Initialisiert Variablen zur Orientierung in Gesamtmatrix
+	call initLineCount(lineCount, offset, edgeLength, mpi_size, mpi_rank)
+	! Eigene Matrix nach Bedarf
+	call allocateMatrix(matrix, mpi_rank, mpi_size, lineCount, edgeLength)
+	! Startbelegung der Matrix
+	call initializeMatrix(matrix, mpi_rank, mpi_size, lineCount, offset)
+	! Warte, bis alles belegt ist
+	
+	!calculating:
+	do i=1,(loopSize + mpi_last)
+		call calculate(matrix, mpi_rank, mpi_master, mpi_last, mpi_ierr, loopSize, i)
+	end do
+	
+	! PROLOG
+	call printMatrix(matrix, interlines, offset, lineCount, mpi_rank, mpi_master, mpi_last, mpi_ierr)
+	
+	! FINAL
+	deallocate(matrix)
+	call MPI_FINALIZE(mpi_ierr)
+
+end program poisson
